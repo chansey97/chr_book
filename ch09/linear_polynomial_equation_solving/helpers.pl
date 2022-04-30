dd82cb3349cf39a37147432ce80016476cbd8767
Siyuan Chen <chansey97@outlook.com>
2022-04-30 22:48:32 +0800

    Modify the normalization code to follow the book's specification.
    1. normalize now is right associative and keeps negative sign.
    
    2. Also delete some unused code.

--- ch09/linear_polynomial_equation_solving/helpers.pl
+++ ch09/linear_polynomial_equation_solving/helpers.pl
@@ -1,32 +1,25 @@
-:- module(helpers, [ eq/2, normalize/2, normalize/3, build/2, mult_vars/3, simplify/2, simplify/3, remove_from_list/3, stringify/3, replace_var/4, replace_var/6 ]).
+%% This file is only used to normalize a polynomial arithmetic expression
+%% Modified from https://github.com/fnogatz/CHR-Linear-Equation-Solver/
+
+:- module(helpers, [ normalize/2, normalize/3, build/3, mult_vars/3, simplify/2, simplify/3, remove_from_list/3]).
 :- use_module(library(chr)).
 
 :- chr_constraint 
 				eq/2, 
 				normalize/2, normalize/3, 
-				build/2, 
+				build/3, 
 				mult_vars/3, 
 				merge_var_abs/3, 
 				simplify/2, simplify/3, 
-				remove_from_list/3, remove_from_list/4, 
-				stringify/3, 
-				replace_var/4, replace_var/6.
+				remove_from_list/3, remove_from_list/4.
 
-
-% eq/2: brings an equation first in the form of eqz(a*X+b*Y+...+u, 0), second in eqz(Vars, Abs), 
-%   i.e. eqz([[X, a], [Y, b], ...], u]).
-%   The eqz/2 will be defined in the submodules.
-eq(X, 0) <=> normalize(X, VarsT, Abs), simplify(VarsT, Vars), eqz(Vars, Abs).
-eq(X, A) <=> number(A) | XA = X-A, eq(XA, 0).
-eq(X, Y) <=> normalize(X-(Y), XY), eq(XY, 0).
-
-
+% N.B. The result normalize/3 now is right associative and keeps negative sign, which follows the book.
 % normalize/2: normalizes a linear combination of linear terms and brings it in the form a*X+b*Y+...+u.
 %   example: normalize(X, R).   -->   R = 1*X+0.
-%   example: normalize(2*(X+12-Y*2)/4, R).   -->   R = 0.5*X-1.0*Y+6.0.
+%   example: normalize(2*(X+12-Y*2)/4, R).   -->   R = 0.5*X+(-1.0*Y+6.0).
 %   example: normalize(2*(X+12-Y*2)/4-X/2, R).   -->   R = -1.0*Y+6.0.
-
-normalize(X, R) <=> normalize(X, Vars, Abs), simplify(Vars, Vars2), build(Vars2, Rv), merge_var_abs(Rv, Abs, R).
+   
+normalize(X, R) <=> normalize(X, Vars, Abs), simplify(Vars, Vars2), build(Vars2, Abs, R).
 
 normalize(-A, Vars, Abs) <=> normalize((-1)*A, Vars, Abs).
 normalize(A-C, Vars, Abs) <=> normalize(A+(-1)*C, Vars, Abs).
@@ -47,25 +40,16 @@ normalize(A*C, Vars, Abs) <=> number(C) | normalize(A, Vars2, Abs2), mult_vars(V
 normalize(X, Vars, Abs) <=> number(X) | Abs is X, Vars = [].
 normalize(X, Vars, Abs) <=> var(X) | Vars = [[X, 1]], Abs = 0.
 
+% N.B. The result build/3 now is right associative and keeps negative sign, which follows the book.
+% build/3
+%   example: build([[A, 1], [B, 2]], R).   -->   R = 2*B+(1* A+6).
+%   example: build([[A, 1], [B, -2]], R).   -->   R = -2*B+(1*A+6).
+%   example: build([[A, -1], [B, 2]], R).   -->   R = 2*B+(-1*A+6).
+%   example: build([[A, 1], [A, 2]], R).   -->   R = 2*A+(1*A+6).
 
-% build/2
-%   example: build([[A, 1], [B, 2]], R).   -->   R = 2*B+1*A.
-%   example: build([[A, 1], [B, -2]], R).   -->   R = -2*B+1*A.
-%   example: build([[A, -1], [B, 2]], R).   -->   R = 2*B-1*A.
-%   example: build([[A, 1], [A, 2]], R).   -->   R = 2*A+1*A.
-build([], R) <=> R is 0.
-build([[V, C]], R) <=> R = C*V.
-build([[V, C]|Rest], R) <=> C<0 | build(Rest, R2), abs(C, Ca), R = R2-Ca*V.
-build([[V, C]|Rest], R) <=> build(Rest, R2), R = R2+C*V.
-
-
-% merge_var_abs/3
-%   example: merge_var_abs(A+2*B, 2, R).   -->    R = A+2*B+2.
-%   example: merge_var_abs(A+2*B, -2, R).   -->    R = A+2*B-2.
-merge_var_abs(0, A, R) <=> R = A.
-merge_var_abs(V, A, R) <=> A<0 | abs(A, Aa), R = V-Aa.
-merge_var_abs(V, A, R) <=> R = V+A.
-
+build(Vars, Abs, R) <=> reverse(Vars, R1), build_(R1, Abs, R).
+build_([], Abs, R) :- !, R is Abs.
+build_([[V, C]|Rest], Abs, R) :- !, build_(Rest, Abs, R2), R = C*V+R2.
 
 % mult_vars/3
 %   example: mult_vars([[A, 1], [B, 2]], 5, R).  -->   R = [[A, 5], [B, 10]].
@@ -99,16 +83,3 @@ remove_from_list([[Var1, Coeff1]|Rest], [Var2, Coeff2], RL, R) <=> remove_from_l
 remove_from_list([], _, RL, R) <=> R = RL.
 
 
-% stringify/3
-stringify(Vars, Abs, R) <=> build(Vars, VarsS), merge_var_abs(VarsS, Abs, RS), normalize(RS, R).
-
-
-% replace_var/4
-%   Note: replace_var(T, S, R, E) <=> S = R, E = T. % would bind S to R.
-%   example: replace_var(2*X+Y-1, X, 3*Y+4, R).   -->   R = 7*Y+7.
-replace_var(Term, S, R, Erg) <=> normalize(Term, Vars, Abs), replace_var(Vars, Abs, S, R, [], ErgN), normalize(ErgN, Erg).
-
-% replace_var/6
-replace_var([], Abs, _, _, RL, Erg) <=> stringify(RL, Abs, Erg).
-replace_var([[Var, Coeff]|Rest], Abs, S, R, RL, Erg) <=> S == Var | append(RL, [[R, Coeff]], RLn), replace_var(Rest, Abs, S, R, RLn, Erg).
-replace_var([[Var, Coeff]|Rest], Abs, S, R, RL, Erg) <=> S \== Var | append(RL, [[Var, Coeff]], RLn), replace_var(Rest, Abs, S, R, RLn, Erg).
